7cc5b8f42bcb06a3db9c9abf7b8319d848fd84ab
SveinnIF <sveinnif@hiof.no>
2022-02-03 10:21:46 +0100

    added a line about the server

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
 <body>
   
 <p>We started with aquiering approval to fork an existing open source project that had some of the basic functionality needed to make the website work.</p>
-<p>source code can be found here:<a href='https://github.com/SveinnIF/User-Management-PHP-MYSQL' >GitHub page</a></p>
-<p></p>
+<p>Source code can be found here: <a href='https://github.com/SveinnIF/User-Management-PHP-MYSQL' > GitHub page</a></p>
+<p>The server was set up in a similar manor as this tutorial: <a href='https://www.cloudbooklet.com/how-to-install-lamp-apache-mysql-php-in-ubuntu-20-04/' > LAMP server tutorial</a> though with a few modifications and additions</p>
 
 </body>
 </html>
