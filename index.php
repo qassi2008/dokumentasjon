f493f6ca57530e6d43c42d44f2f379b13f503f4c
SveinnIF <sveinnif@hiof.no>
2022-02-03 09:28:33 +0100

    fist commit, basic html and simple info

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en" class="no-js">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+</head>
+<body>
+  
+<p>We started with aquiering approval to fork an existing open source project that had some of the basic functionality needed to make the website work.</p>
+<p>source code can be found here:<a href='https://github.com/SveinnIF/User-Management-PHP-MYSQL' >GitHub page</a></p>
+<p></p>
+
+</body>
+</html>
