8d084566d3951098803bcbaaef43cb43900687a6
SveinnIF <sveinnif@hiof.no>
2022-02-03 10:37:33 +0100

    forgotten password system

--- index.php
+++ index.php
@@ -21,10 +21,15 @@
 <p>Sveinn Frimannsson: sveinnif@hiof.no</p>
 <p>så kan dere andre legge til deres her også</p>
 
+<h2>
+  How the website was made:
+</h2>
 <p>We started with aquiering approval to fork an existing open source project that had some of the basic functionality needed to make the website work.</p>
 <p>Source code can be found here: <a href='https://github.com/SveinnIF/User-Management-PHP-MYSQL' > GitHub page.</a></p>
 <p>The server was set up in a similar manor as the <a href='https://www.cloudbooklet.com/how-to-install-lamp-apache-mysql-php-in-ubuntu-20-04/' > LAMP server tutorial</a> though with a few modifications and additions.</p>
-
+<h3>The forgotten password system:</h3>
+<p>The forgotten password page was created by modifying some existing code found on the internet, then taking some code that was part of the original site and forcing it to work as we wanted.</p>
+<p> Now it asks for forgotten password if the lecturer has written the wrong password once, the lecturer is then prompted to input their mail address after which they will get a mail with a custom link that takes them to the reset page where they can input a new password.</p>
 
 </body>
 </html>
