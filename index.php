981f3c8ce28efc7bf664e32e201dc031fc1c716b
SveinnIF <sveinnif@hiof.no>
2022-02-03 10:29:24 +0100

    contact info

--- index.php
+++ index.php
@@ -10,9 +10,21 @@
 </head>
 <body>
   
+  <h1>
+    Documentation for Group 1.
+  </h1>
+
+<h2>
+  Contact information:
+</h2>
+
+<p>Sveinn Frimannsson: sveinnif@hiof.no</p>
+<p>så kan dere andre legge til deres her også</p>
+
 <p>We started with aquiering approval to fork an existing open source project that had some of the basic functionality needed to make the website work.</p>
-<p>Source code can be found here: <a href='https://github.com/SveinnIF/User-Management-PHP-MYSQL' > GitHub page</a></p>
-<p>The server was set up in a similar manor as this tutorial: <a href='https://www.cloudbooklet.com/how-to-install-lamp-apache-mysql-php-in-ubuntu-20-04/' > LAMP server tutorial</a> though with a few modifications and additions</p>
+<p>Source code can be found here: <a href='https://github.com/SveinnIF/User-Management-PHP-MYSQL' > GitHub page.</a></p>
+<p>The server was set up in a similar manor as the <a href='https://www.cloudbooklet.com/how-to-install-lamp-apache-mysql-php-in-ubuntu-20-04/' > LAMP server tutorial</a> though with a few modifications and additions.</p>
+
 
 </body>
 </html>
